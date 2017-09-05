3e7b069b3bded5660490b1d54cc228696141adf3
Michael Smith <michaeljsmith8@ufl.edu>
2017-09-04 22:08:02 -0400

    assignment2

--- listingController.js
+++ listingController.js
@@ -1,14 +1,30 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
+
+    //listings are all the listings I think
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+
+
+    $scope.addListing = function() {
+      $scope.listings.push($scope.x);
+
+    	$scope.x='';
+
+
+
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+
+    };
+    $scope.showDetails = function(index) {
+        $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
